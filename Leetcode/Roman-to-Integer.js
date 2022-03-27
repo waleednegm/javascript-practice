30361549c2f2a8d6cdf7b58113e6e92786dd8a6a
Waleed Negm <55960681+waleednegm@users.noreply.github.com>
2022-03-27 21:13:29 +0200

    Create Roman-to-Integer.js

---
+++ Leetcode/Roman-to-Integer.js
@@ -0,0 +1,16 @@
+#Leetcode
+#problem https://leetcode.com/problems/roman-to-integer/
+
+var romanToInt = function(s) {
+    let context = {'I':1 , 'V':5 ,'X':10 ,'L':50 ,'C':100 ,'D': 500,'M': 1000 };
+    let counter=0;
+    for(let i=0;i<s.length-1;i++){
+        if(context[s[i]]>=context[s[i+1]]){
+            counter+=context[s[i]];
+        }else{
+            counter-=context[s[i]];
+        }
+    }
+    counter+=context[s[s.length-1]];
+    return counter;
+};
