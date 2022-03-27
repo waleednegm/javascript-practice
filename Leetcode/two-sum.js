e92568dcb59bb48dab851c4b060e91983af5ceb6
Waleed Negm <55960681+waleednegm@users.noreply.github.com>
2022-03-27 21:10:24 +0200

    Create two-sum.js

---
+++ Leetcode/two-sum.js
@@ -0,0 +1,14 @@
+#Leetcode
+#problem https://leetcode.com/problems/two-sum/
+
+var twoSum = function(nums, target) {
+    let sum=0;
+    for( let i=0;i<nums.length-1;i++ ){
+        for(let j=0;j<nums.length;j++){
+            sum = nums[i]+nums[j];
+            if(sum==target & i != j){
+                return [i,j];
+            }
+        }
+    }
+};
