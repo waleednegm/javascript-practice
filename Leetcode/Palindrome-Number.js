969d91a7ca84da41bbe30f6bddab358f6caa3674
Waleed Negm <55960681+waleednegm@users.noreply.github.com>
2022-03-27 21:12:28 +0200

    Create Palindrome-Number.js

---
+++ Leetcode/Palindrome-Number.js
@@ -0,0 +1,22 @@
+#Leetcode
+#problem https://leetcode.com/problems/palindrome-number/
+
+var isPalindrome = function(x) {
+    let num = x.toString();
+    let size = num.length;
+    size=Math.ceil(size/2);
+    console.log(size);
+    let b=true;
+    let xx=num.length -1;
+    if(num.length==1){
+        return true;
+    }
+    for(let i=0;i<size;i++){
+        if(num[i]!=num[xx]){
+            return false;
+        }
+        xx--;
+        
+    }
+    return true;
+};
